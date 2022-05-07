60a84af605aaf83a554c8571bab5fd6c81c83fa3
Francisco <fcisco.dev@gmail.com>
2022-05-07 19:58:17 +0700

    feature: create controller for GetAllProducts

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -5,21 +5,52 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
-	"github.com/zipzap11/pharm-API/db"
+	resp "github.com/zipzap11/pharm-API/dto/response"
 	"github.com/zipzap11/pharm-API/model"
 )
 
-type ProductController struct{}
+type ProductController struct {
+	productUsecase model.ProductUsecase
+}
 
-func (c *ProductController) GetAllProducts(ctx echo.Context) error {
-	var result []model.Product
-	err := db.DB.Model(&model.Product{}).Find(&result).Error
+func NewProductController(productUsecase model.ProductUsecase) *ProductController {
+	return &ProductController{
+		productUsecase: productUsecase,
+	}
+}
+
+func (pc *ProductController) GetAllProducts(c echo.Context) error {
+	categoryID, err := getInt64FromQuery("category", c)
 	if err != nil {
 		logrus.Error(err)
-		return ctx.String(http.StatusInternalServerError, "internal server error")
+		return c.JSON(http.StatusBadRequest, resp.ErrResponse{
+			Message: "invalid category",
+		})
 	}
-	return ctx.JSON(http.StatusOK, map[string]interface{}{
-		"message": "ok",
-		"data":    result,
+	sort := c.QueryParam("sort")
+	sortType, err := model.ParseProductSortType(sort)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, resp.ErrResponse{
+			Message: "invalid sort type",
+		})
+	}
+
+	query := c.QueryParam("query")
+
+	result, err := pc.productUsecase.GetAllProducts(c.Request().Context(), &model.SortFilter{
+		CategoryID: int64(categoryID),
+		SortType:   sortType,
+		Query:      query,
+	})
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(GetErrorCode(err), resp.ErrResponse{
+			Message: err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, resp.StdResponse{
+		Message: "ok",
+		Data:    result,
 	})
 }
