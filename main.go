b08216f87f912d78474c35511c1c86f79c1b442b
Francisco <fcisco.dev@gmail.com>
2022-05-07 19:59:54 +0700

    feature: dependency injection on product domain

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/zipzap11/pharm-API/controller"
 	"github.com/zipzap11/pharm-API/db"
+	"github.com/zipzap11/pharm-API/repository"
+	"github.com/zipzap11/pharm-API/usecase"
 )
 
 func main() {
 	e := echo.New()
 	db.InitDB()
-	productController := controller.ProductController{}
+
+	// product
+	productRepository := repository.NewProductRepository(db.DB)
+	productUsecase := usecase.NewProductUsecase(productRepository)
+	productController := controller.NewProductController(productUsecase)
+
+	// middleware
 	e.Use(middleware.Logger())
+
+	// path
 	e.GET("/products", productController.GetAllProducts)
+
 	e.Start("localhost:8000")
 }
