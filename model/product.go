93c6ea45668daf271883064efdb25ea7ef5052cd
Francisco <fcisco.dev@gmail.com>
2022-05-07 19:56:24 +0700

    feature: create product usecase and repo and product sort type

--- model/product.go
+++ model/product.go
@@ -1,6 +1,11 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
@@ -9,4 +14,39 @@ type Product struct {
 	Price       int64
 	Weight      int64
 	Description string
+}
+
+type SortFilter struct {
+	CategoryID int64
+	SortType   ProductSortType
+	Query      string
+}
+
+type ProductUsecase interface {
+	GetAllProducts(ctx context.Context, sortFilter *SortFilter) ([]*Product, error)
+}
+
+type ProductRepository interface {
+	GetAllProducts(ctx context.Context, sortFilter *SortFilter) ([]*Product, error)
+}
+
+type ProductSortType string
+
+const (
+	SortProductAsc  ProductSortType = "ASC"
+	SortProductDesc ProductSortType = "DESC"
+	SortProductNone ProductSortType = "NONE"
+)
+
+func ParseProductSortType(sortType string) (ProductSortType, error) {
+	switch {
+	case sortType == "asc":
+		return SortProductAsc, nil
+	case sortType == "desc":
+		return SortProductDesc, nil
+	case sortType == "":
+		return SortProductNone, nil
+	default:
+		return "", errors.New("invalid sort type")
+	}
 }
