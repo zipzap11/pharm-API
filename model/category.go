06221f46ba16962c26cdbae0f45e8897edd32e6e
Francisco <fcisco.dev@gmail.com>
2022-05-08 12:04:56 +0700

    feature: create GetAllCategories on usecase and repository

--- model/category.go
+++ model/category.go
@@ -1,8 +1,20 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
 	gorm.Model
 	Name string
+}
+
+type CategoryUsecase interface {
+	GetAllCategories(ctx context.Context) ([]*Category, error)
+}
+
+type CategoryRepository interface {
+	GetAllCategories(ctx context.Context) ([]*Category, error)
 }
