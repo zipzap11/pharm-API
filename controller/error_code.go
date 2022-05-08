677455962deda79e444d3747a466f27b11d3c375
Francisco <fcisco.dev@gmail.com>
2022-05-08 12:04:17 +0700

    feature: add not found error http status

--- controller/error_code.go
+++ controller/error_code.go
@@ -1,9 +1,15 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/zipzap11/pharm-API/usecase"
+)
 
 func GetErrorCode(err error) int {
 	switch err {
+	case usecase.ErrNotFound:
+		return http.StatusNotFound
 	default:
 		return http.StatusInternalServerError
 	}
