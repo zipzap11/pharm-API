115990bf95d237e185b69e1d6018577bb1595071
Francisco <fcisco.dev@gmail.com>
2022-05-09 19:06:02 +0700

    feature: paseto token provider impl

---
+++ util/paseto.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type PasetoProvider struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPasetoProvider(symmetricKey string) (TokenProvider, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	return &PasetoProvider{
+		symmetricKey: []byte(symmetricKey),
+		paseto:       paseto.NewV2(),
+	}, nil
+}
+
+func (p *PasetoProvider) CreateToken(userID int64, duration time.Duration) (string, *Payload, error) {
+	payload, err := NewPayload(userID, duration)
+	if err != nil {
+		return "", nil, err
+	}
+	token, err := p.paseto.Encrypt(p.symmetricKey, payload, nil)
+	if err != nil {
+		return "", nil, err
+	}
+	return token, payload, nil
+}
+
+func (p *PasetoProvider) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+	err := p.paseto.Decrypt(token, p.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
