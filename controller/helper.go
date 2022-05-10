f895359b05724005d6ff88cf886929acb020fbfa
Francisco <fcisco.dev@gmail.com>
2022-05-10 21:02:38 +0700

    feature: create response constructor

--- controller/helper.go
+++ controller/helper.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
+	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	resp "github.com/zipzap11/pharm-API/dto/response"
 )
 
 func getInt64FromQuery(key string, c echo.Context) (int64, error) {
@@ -16,4 +18,23 @@ func getInt64FromQuery(key string, c echo.Context) (int64, error) {
 		return 0, err
 	}
 	return int64(res), nil
+}
+
+func ErrResponse(c echo.Context, err error) error {
+	return c.JSON(GetErrorCode(err), resp.ErrResponse{
+		Message: err.Error(),
+	})
+}
+
+func ErrResponseWithCode(c echo.Context, err error, code int) error {
+	return c.JSON(code, resp.ErrResponse{
+		Message: err.Error(),
+	})
+}
+
+func SuccessResponse(c echo.Context, data interface{}) error {
+	return c.JSON(http.StatusOK, resp.StdResponse{
+		Message: "ok",
+		Data:    data,
+	})
 }
