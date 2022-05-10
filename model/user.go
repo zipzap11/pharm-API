18e79805e4b124233d97b3f124a1ca5a824762a9
Francisco <fcisco.dev@gmail.com>
2022-05-10 21:00:22 +0700

    feature: add user repo and usecase

--- model/user.go
+++ model/user.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -8,4 +12,14 @@ type User struct {
 	Email    string
 	Password string
 	Phone    string
+}
+
+type UserUsecase interface {
+	CreateUser(ctx context.Context, user *User) error
+	Login(ctx context.Context, email string, password string) (string, string, error)
+}
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, user *User) error
+	FindUserByEmail(ctx context.Context, email string) (*User, error)
 }
