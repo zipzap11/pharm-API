562138d5026bf7b2480c467c34003a650491c11b
Francisco <fcisco.dev@gmail.com>
2022-05-10 21:00:55 +0700

    feature: create session model repo and usecase

---
+++ model/session.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Session struct {
+	ID           uuid.UUID
+	RefreshToken string
+	UserID       int64
+	IsBlocked    bool
+	CreatedAt    time.Time
+	ExpiredAt    time.Time
+}
+
+type SessionRepository interface {
+	CreateSession(ctx context.Context, session *Session) error
+	GetSessionByID(ctx context.Context, id uuid.UUID) (*Session, error)
+}
+
+type SessionUsecase interface {
+	RefreshSession(ctx context.Context, token string) (string, string, error)
+}
