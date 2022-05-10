9645ab25dd4b0ff7d6e17486d413926b44b9d8e5
Francisco <fcisco.dev@gmail.com>
2022-05-10 21:01:11 +0700

    feature: add more error type

--- usecase/error.go
+++ usecase/error.go
@@ -3,7 +3,10 @@ package usecase
 import "errors"
 
 var (
-	ErrNotFound          = errors.New("not found")
-	ErrInvalidEmail      = errors.New("email is invalid")
-	ErrInvalidCredential = errors.New("wrong email or password")
+	ErrNotFound           = errors.New("not found")
+	ErrInvalidEmail       = errors.New("email is invalid")
+	ErrInvalidCredential  = errors.New("wrong email or password")
+	ErrIncorrectUserToken = errors.New("wrong user")
+	ErrBlockedSession     = errors.New("session is blocked")
+	ErrMissmatchedToken   = errors.New("missmatched token")
 )
