b35a7f24e6b021ccba1131d06dc514bf20aba9e5
Francisco <fcisco.dev@gmail.com>
2022-05-11 20:50:34 +0700

    feature: create cart repo and cart usecase

--- model/cart.go
+++ model/cart.go
@@ -1,8 +1,21 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type Cart struct {
 	gorm.Model
 	UserID int64
+	Items  []*CartItem `gorm:"-:all"`
+}
+
+type CartRepository interface {
+	FindCartByUserID(ctx context.Context, userID int64) (*Cart, error)
+}
+
+type CartUsecase interface {
+	FindCartByUserID(ctx context.Context, userID int64) (*Cart, error)
 }
