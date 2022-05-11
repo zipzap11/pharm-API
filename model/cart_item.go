7eebc6b658cf17647156df32b67efd794dec5734
Francisco <fcisco.dev@gmail.com>
2022-05-11 20:50:53 +0700

    feature: create cart item repo

--- model/cart_item.go
+++ model/cart_item.go
@@ -1,10 +1,18 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type CartItem struct {
 	gorm.Model
 	Quantity  int64
 	ProductID int64
 	CartID    int64
+}
+
+type CartItemRepository interface {
+	GetItemsByCartID(ctx context.Context, cartID int64) (items []*CartItem, err error)
 }
